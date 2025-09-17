03e3f54e9da0dfa3d966fd18895a27f13ffacc45
Grant Willison <GWWillisonBusiness@gmail.com>
2025-09-17 00:13:24 -0400

    Comment Out Code

--- src/server/server.js
+++ src/server/server.js
@@ -22,13 +22,14 @@ app.get("/api/ev-results", async (_req, res) => {
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+/**
 app.use(express.static(path.join(__dirname, "../build")));
 
 // Fallback to React's index.html for unknown routes
 app.use((req, res) => {
   res.sendFile(path.join(__dirname, "../build", "index.html"));
 });
-
+ */
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`✅ Server running on http://localhost:${PORT}`);
